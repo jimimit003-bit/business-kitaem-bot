955abc5f2440e4a150bf63e07270d211b8b5fa27
jimimit003-bit <jimimit003@gmail.com>
2026-03-13 11:38:17 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -156,6 +156,7 @@ view_state = {}
 # =========================
 def now_ts() -> int:
     return int(time.time())
+    
 def go_main_menu(chat_id: int, text: str = "Главное меню:"):
     pending_search.discard(chat_id)
     bot.send_message(chat_id, text, reply_markup=main_menu())
